94ecdef0375f40930229b5e3cdc8a33e2977c0fb
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-11-22 16:04:28 -0600

    Added data subdirectory; added some info on PBMC Zheng et al data set.

---
+++ analysis/pbmc.R
@@ -0,0 +1,22 @@
+# Notes on Zheng et al PBMC data:
+#
+#  + Downloaded "Fresh 68k PBMCs (Donor A)" data set from:
+#    https://support.10xgenomics.com/single-cell-gene-expression/datasets
+#
+#  + Specifically, I downloaded the "Gene/cell matrix (raw)" file.
+#
+#  + Rename files as:
+#    fresh_68k_pbmc_donor_barcodes.tsv
+#    fresh_68k_pbmc_donor_genes.tsv
+#    fresh_68k_pbmc_donor_matrix.mtx
+#
+#  + A summary of the data is given here:
+#    http://cf.10xgenomics.com/samples/cell-exp/1.1.0/fresh_68k_pbmc_donor_a/fresh_68k_pbmc_donor_a_web_summary.html
+#   
+#  + Data summary:
+#    n = 68,579
+#    mean reads per cell = 20,491
+#    median read per cell = 1,292
+#    median genes per cell = 525
+#    number of reads = 1,405,303,609
+#
