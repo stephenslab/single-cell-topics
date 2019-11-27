dc4cd6296621f0e48d655af767667efc74288672
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-11-26 22:34:28 -0600

    Updated notes on 68k PBMC data.

--- analysis/pbmc.R
+++ analysis/pbmc.R
@@ -3,20 +3,23 @@
 #  + Downloaded "Fresh 68k PBMCs (Donor A)" data set from:
 #    https://support.10xgenomics.com/single-cell-gene-expression/datasets
 #
-#  + Specifically, I downloaded the "Gene/cell matrix (raw)" file.
+#  + Associated publication: Zheng et al (2017). Massively parallel
+#    digital transcriptional profiling of single cells. Nature
+#    Communications 8, 14049. doi:10.1038/ncomms14049
 #
-#  + Rename files as:
-#    fresh_68k_pbmc_donor_barcodes.tsv
-#    fresh_68k_pbmc_donor_genes.tsv
-#    fresh_68k_pbmc_donor_matrix.mtx
+#  + Specifically, I downloaded the "Gene/cell matrix (filtered)"
+#    tar.gz file. Then I moved the files to
+#    data/fresh_68k_pbmc_donor_a_filtered and compressed them with gzip.
 #
 #  + A summary of the data is given here:
 #    http://cf.10xgenomics.com/samples/cell-exp/1.1.0/fresh_68k_pbmc_donor_a/fresh_68k_pbmc_donor_a_web_summary.html
 #   
-#  + Data summary:
+#  + These are the numbers given from that webpage:
 #    n = 68,579
 #    mean reads per cell = 20,491
 #    median read per cell = 1,292
 #    median genes per cell = 525
 #    number of reads = 1,405,303,609
 #
+library(Matrix)
+library(readr)
