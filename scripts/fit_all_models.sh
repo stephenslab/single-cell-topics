505d20eb8d61dd92ac60703b1fcc6fcbe46871ad
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-08-06 15:56:58 -0500

    Added droplet data fits; adjusted fit_poisson_nmf sbatch configuration.

--- scripts/fit_all_models.sh
+++ scripts/fit_all_models.sh
@@ -76,6 +76,83 @@ sbatch ${SCRIPT_FIT} droplet  2 em     1000 yes fit-droplet-em-ex-k=2
 sbatch ${SCRIPT_FIT} droplet  2 ccd    1000 yes fit-droplet-ccd-ex-k=2
 sbatch ${SCRIPT_FIT} droplet  2 scd    1000 yes fit-droplet-scd-ex-k=2
 
+sbatch ${SCRIPT_FIT} droplet  3 em     1000  no fit-droplet-em-k=3
+sbatch ${SCRIPT_FIT} droplet  3 ccd    1000  no fit-droplet-ccd-k=3
+sbatch ${SCRIPT_FIT} droplet  3 scd    1000  no fit-droplet-scd-k=3
+sbatch ${SCRIPT_FIT} droplet  3 em     1000 yes fit-droplet-em-ex-k=3
+sbatch ${SCRIPT_FIT} droplet  3 ccd    1000 yes fit-droplet-ccd-ex-k=3
+sbatch ${SCRIPT_FIT} droplet  3 scd    1000 yes fit-droplet-scd-ex-k=3
+
+sbatch ${SCRIPT_FIT} droplet  4 em     1000  no fit-droplet-em-k=4
+sbatch ${SCRIPT_FIT} droplet  4 ccd    1000  no fit-droplet-ccd-k=4
+sbatch ${SCRIPT_FIT} droplet  4 scd    1000  no fit-droplet-scd-k=4
+sbatch ${SCRIPT_FIT} droplet  4 em     1000 yes fit-droplet-em-ex-k=4
+sbatch ${SCRIPT_FIT} droplet  4 ccd    1000 yes fit-droplet-ccd-ex-k=4
+sbatch ${SCRIPT_FIT} droplet  4 scd    1000 yes fit-droplet-scd-ex-k=4
+
+sbatch ${SCRIPT_FIT} droplet  5 em     1000  no fit-droplet-em-k=5
+sbatch ${SCRIPT_FIT} droplet  5 ccd    1000  no fit-droplet-ccd-k=5
+sbatch ${SCRIPT_FIT} droplet  5 scd    1000  no fit-droplet-scd-k=5
+sbatch ${SCRIPT_FIT} droplet  5 em     1000 yes fit-droplet-em-ex-k=5
+sbatch ${SCRIPT_FIT} droplet  5 ccd    1000 yes fit-droplet-ccd-ex-k=5
+sbatch ${SCRIPT_FIT} droplet  5 scd    1000 yes fit-droplet-scd-ex-k=5
+
+sbatch ${SCRIPT_FIT} droplet  6 em     1000  no fit-droplet-em-k=6
+sbatch ${SCRIPT_FIT} droplet  6 ccd    1000  no fit-droplet-ccd-k=6
+sbatch ${SCRIPT_FIT} droplet  6 scd    1000  no fit-droplet-scd-k=6
+sbatch ${SCRIPT_FIT} droplet  6 em     1000 yes fit-droplet-em-ex-k=6
+sbatch ${SCRIPT_FIT} droplet  6 ccd    1000 yes fit-droplet-ccd-ex-k=6
+sbatch ${SCRIPT_FIT} droplet  6 scd    1000 yes fit-droplet-scd-ex-k=6
+
+sbatch ${SCRIPT_FIT} droplet  7 em     1000  no fit-droplet-em-k=7
+sbatch ${SCRIPT_FIT} droplet  7 ccd    1000  no fit-droplet-ccd-k=7
+sbatch ${SCRIPT_FIT} droplet  7 scd    1000  no fit-droplet-scd-k=7
+sbatch ${SCRIPT_FIT} droplet  7 em     1000 yes fit-droplet-em-ex-k=7
+sbatch ${SCRIPT_FIT} droplet  7 ccd    1000 yes fit-droplet-ccd-ex-k=7
+sbatch ${SCRIPT_FIT} droplet  7 scd    1000 yes fit-droplet-scd-ex-k=7
+
+sbatch ${SCRIPT_FIT} droplet  8 em     1000  no fit-droplet-em-k=8
+sbatch ${SCRIPT_FIT} droplet  8 ccd    1000  no fit-droplet-ccd-k=8
+sbatch ${SCRIPT_FIT} droplet  8 scd    1000  no fit-droplet-scd-k=8
+sbatch ${SCRIPT_FIT} droplet  8 em     1000 yes fit-droplet-em-ex-k=8
+sbatch ${SCRIPT_FIT} droplet  8 ccd    1000 yes fit-droplet-ccd-ex-k=8
+sbatch ${SCRIPT_FIT} droplet  8 scd    1000 yes fit-droplet-scd-ex-k=8
+
+sbatch ${SCRIPT_FIT} droplet  9 em     1000  no fit-droplet-em-k=9
+sbatch ${SCRIPT_FIT} droplet  9 ccd    1000  no fit-droplet-ccd-k=9
+sbatch ${SCRIPT_FIT} droplet  9 scd    1000  no fit-droplet-scd-k=9
+sbatch ${SCRIPT_FIT} droplet  9 em     1000 yes fit-droplet-em-ex-k=9
+sbatch ${SCRIPT_FIT} droplet  9 ccd    1000 yes fit-droplet-ccd-ex-k=9
+sbatch ${SCRIPT_FIT} droplet  9 scd    1000 yes fit-droplet-scd-ex-k=9
+
+sbatch ${SCRIPT_FIT} droplet  10 em     1000  no fit-droplet-em-k=10
+sbatch ${SCRIPT_FIT} droplet  10 ccd    1000  no fit-droplet-ccd-k=10
+sbatch ${SCRIPT_FIT} droplet  10 scd    1000  no fit-droplet-scd-k=10
+sbatch ${SCRIPT_FIT} droplet  10 em     1000 yes fit-droplet-em-ex-k=10
+sbatch ${SCRIPT_FIT} droplet  10 ccd    1000 yes fit-droplet-ccd-ex-k=10
+sbatch ${SCRIPT_FIT} droplet  10 scd    1000 yes fit-droplet-scd-ex-k=10
+
+sbatch ${SCRIPT_FIT} droplet  11 em     1000  no fit-droplet-em-k=11
+sbatch ${SCRIPT_FIT} droplet  11 ccd    1000  no fit-droplet-ccd-k=11
+sbatch ${SCRIPT_FIT} droplet  11 scd    1000  no fit-droplet-scd-k=11
+sbatch ${SCRIPT_FIT} droplet  11 em     1000 yes fit-droplet-em-ex-k=11
+sbatch ${SCRIPT_FIT} droplet  11 ccd    1000 yes fit-droplet-ccd-ex-k=11
+sbatch ${SCRIPT_FIT} droplet  11 scd    1000 yes fit-droplet-scd-ex-k=11
+
+sbatch ${SCRIPT_FIT} droplet  12 em     1000  no fit-droplet-em-k=12
+sbatch ${SCRIPT_FIT} droplet  12 ccd    1000  no fit-droplet-ccd-k=12
+sbatch ${SCRIPT_FIT} droplet  12 scd    1000  no fit-droplet-scd-k=12
+sbatch ${SCRIPT_FIT} droplet  12 em     1000 yes fit-droplet-em-ex-k=12
+sbatch ${SCRIPT_FIT} droplet  12 ccd    1000 yes fit-droplet-ccd-ex-k=12
+sbatch ${SCRIPT_FIT} droplet  12 scd    1000 yes fit-droplet-scd-ex-k=12
+
+sbatch ${SCRIPT_FIT} droplet  13 em     1000  no fit-droplet-em-k=13
+sbatch ${SCRIPT_FIT} droplet  13 ccd    1000  no fit-droplet-ccd-k=13
+sbatch ${SCRIPT_FIT} droplet  13 scd    1000  no fit-droplet-scd-k=13
+sbatch ${SCRIPT_FIT} droplet  13 em     1000 yes fit-droplet-em-ex-k=13
+sbatch ${SCRIPT_FIT} droplet  13 ccd    1000 yes fit-droplet-ccd-ex-k=13
+sbatch ${SCRIPT_FIT} droplet  13 scd    1000 yes fit-droplet-scd-ex-k=13
+
 #                    data           k method    n  ex outfile
-sbatch ${SCRIPT_FIT} pbmc_purified 13 scd      40 yes fit-pbmc-purified-scd-ex-k-13
-sbatch ${SCRIPT_FIT} pulseseq      13 scd      40 yes fit-pulseseq-scd-ex-k-13
+sbatch ${SCRIPT_FIT} pbmc_purified 13 scd    1000 yes fit-pbmc-purified-scd-ex-k-13
+sbatch ${SCRIPT_FIT} pulseseq      13 scd    1000 yes fit-pulseseq-scd-ex-k-13
