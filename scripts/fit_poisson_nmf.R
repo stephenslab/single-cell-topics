48f17b73d67df1dd6a429fe97c902926b132c8ba
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-08-05 22:18:53 -0500

    Working on R and sbatch scripts for fitting Poisson NMF models to UMI counts.

--- scripts/fit_poisson_nmf.R
+++ scripts/fit_poisson_nmf.R
@@ -29,32 +29,29 @@ library(fastTopics)
 # Process the command-line arguments.
 parser <- OptionParser()
 parser <- add_option(parser,"--counts",type="character",default="counts.RData")
+parser <- add_option(parser,"--prefit",type = "character",default="prefix.rds")
 parser <- add_option(parser,c("--out","-o"),type="character",default="out.rds")
-parser <- add_option(parser,"--prefit",type = "character",default = "auto")
-parser <- add_option(parser,c("--k","-k"),type = "integer",default = 3)
 parser <- add_option(parser,"--method",type = "character",default = "em")
-parser <- add_option(parser,"--nc",type = "integer",default = 1)
+parser <- add_option(parser,c("--numiter","-n"),type="integer",default=1000)
 parser <- add_option(parser,"--extrapolate",action = "store_true")
-parser <- add_option(parser,"--numiter",type = "integer",default = 1000)
+parser <- add_option(parser,"--nc",type = "integer",default = 1)
 out    <- parse_args(parser)
 countsfile  <- out$counts
+prefitfile  <- out$prefit
 outfile     <- out$out
-prefit.file <- out$prefit
-k           <- out$k
 method      <- out$method
-nc          <- out$nc
 numiter     <- out$numiter
 extrapolate <- !is.null(out$extrapolate)
+nc          <- out$nc
 rm(parser,out)
 
 print(countsfile)
-print(outfile)
 print(prefitfile)
-print(k)
+print(outfile)
 print(method)
-print(nc)
 print(numiter)
 print(extrapolate)
+print(nc)
 
 # Initialize the sequence of pseudorandom numbers.
 set.seed(1)
@@ -68,16 +65,15 @@ cat(sprintf("Loaded %d x %d counts matrix.\n",nrow(counts),ncol(counts)))
 
 # LOAD PRE-FITTED MODEL
 # ---------------------
-cat(sprintf("Loading pre-fitted model from %s\n",prefit.file))
-fit0 <- readRDS(prefit.file)$fit
+cat(sprintf("Loading pre-fitted model from %s\n",prefitfile))
+fit0 <- readRDS(prefitfile)$fit
 
 # FIT POISSON NON-NEGATIVE MATRIX FACTORIZATION
 # ---------------------------------------------
 # Now we are ready to perform the main model-fitting step.
 cat("Fitting Poisson NMF to count data.\n")
-control <- list(extrapolate = extrapolate,nc = nc)
-if (method != "ccd")
-  control$numiter <- 4
+control <- list(extrapolate = extrapolate,nc = nc,
+                numiter = ifelse(method == "ccd",1,4))
 timing <- system.time({
   fit <- fit_poisson_nmf(counts,fit0 = fit0,numiter = numiter,
                          method = method,control = control)
@@ -87,7 +83,7 @@ cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
 # SAVE RESULTS
 # ------------
 cat("Saving results.\n")
-saveRDS(list(k = k,method = method,control = control,fit = fit),
+saveRDS(list(method = method,control = control,fit = fit),
         file = outfile)
 
 # SESSION INFO
