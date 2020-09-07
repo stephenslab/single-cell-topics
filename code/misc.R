c7b9e5d007db213386cc72cca5f01bad27327ec4
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-09-06 22:00:06 -0500

    I have a rough draft of the b-cell plots; I know need to incorporate these plots into plots_pbmc.R.

--- code/misc.R
+++ code/misc.R
@@ -3,8 +3,8 @@
 # diff_count_analysis output matrices).
 select_diff_count_res <- function (diff_count_res, genes) {
   diff_count_res$colmeans <- diff_count_res$colmeans[genes]
-  diff_count_res$F0       <- diff_count_res$F0[genes.]
-  diff_count_res$F1       <- diff_count_res$F1[genes.]
+  diff_count_res$F0       <- diff_count_res$F0[genes,]
+  diff_count_res$F1       <- diff_count_res$F1[genes,]
   diff_count_res$pval     <- diff_count_res$pval[genes,]
   diff_count_res$beta     <- diff_count_res$beta[genes,]
   diff_count_res$se       <- diff_count_res$se[genes,]
