1d0f457735b62b561b72068ac40f2ae61308d2db
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-09-11 23:36:43 -0500

    Added some volcano plots for 68k data to yet_more_pbmc.R.

--- analysis/yet_more_pbmc.R
+++ analysis/yet_more_pbmc.R
@@ -13,6 +13,27 @@ fit_clusters_68k <-
 diff_count_clusters_68k <- diff_count_analysis(fit_clusters_68k,counts_68k)
 
 # Volcano plot for B-cells.
-p1 <- volcano_plot(diff_count_clusters_68k,k = "A2",labels = genes_68k$symbol,
-                   label_above_quantile = 0.995)
+p1 <- volcano_plot(diff_count_clusters_68k,k = "B",labels = genes_68k$symbol,
+                   label_above_quantile = 0.998)
 print(p1)
+
+# Volcano plot for natural killer (NK) cells.
+p2 <- volcano_plot(diff_count_clusters_68k,k = "NK",labels = genes_68k$symbol,
+                   label_above_quantile = 0.995)
+print(p2)
+
+# Volcano plot for CD14+ monocytes.
+p3 <- volcano_plot(diff_count_clusters_68k,k = "CD14+",
+                   labels = genes_68k$symbol,
+                   label_above_quantile = 0.997)
+print(p3)
+
+# Volcano plot for dendritic cells.
+p4 <- volcano_plot(diff_count_clusters_68k,k = "D",labels = genes_68k$symbol,
+                   label_above_quantile = 0.995)
+print(p4)
+
+# Volcano plot for cluster C.
+p5 <- volcano_plot(diff_count_clusters_68k,k = "C",labels = genes_68k$symbol,
+                   label_above_quantile = 0.99)
+print(p5)
