14d13bd41feb8a308886f81edf0257f6bd748adc
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-09-28 15:54:56 -0500

    Added plots to temp2.R.

--- scripts/temp2.R
+++ scripts/temp2.R
@@ -1,2 +1,8 @@
+# Droplet.
 loadings_plot(poisson2multinom(fit),samples$tissue)
 pca_plot(poisson2multinom(fit),pcs = 1:2)
+
+# Pulse-seq.
+sum(loglik_poisson_nmf(counts,fit))
+sum(loglik_poisson_nmf(counts,fits[["fit-pulseseq-scd-ex-k=13"]]))
+loadings_plot(poisson2multinom(fit),samples$tissue)
