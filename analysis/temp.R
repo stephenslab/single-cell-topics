29e727ce49a82c1632e178214e71f21939da7491
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-10-01 09:10:31 -0500

    Minor edits.

--- analysis/temp.R
+++ analysis/temp.R
@@ -4,5 +4,5 @@ res  <- diff_count_analysis(fit2,counts[rows,])
 pca_plot(fit2,k = c(4,5,7))
 pca_plot_with_counts(fit2,counts[rows,"Muc5b"],pcs = 1:2,log = TRUE)
 res$colmeans <- res$colmeans + 1e-4
-plot_grid(volcano_plot(res,k = 5)
+plot_grid(volcano_plot(res,k = 5),
           volcano_plot(res,k = 7))
