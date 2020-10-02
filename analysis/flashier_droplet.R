cf158bd8646aa26304e3600f6923bbf8bd965808
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-10-02 12:34:06 -0500

    Made some improvements to flashier_droplet.R.

--- analysis/flashier_droplet.R
+++ analysis/flashier_droplet.R
@@ -5,13 +5,19 @@ library(ggplot2)
 library(cowplot)
 set.seed(1)
 
+# Script parameters.
+# flash_prior <- list(prior.point.normal(),prior.point.normal())
+# flash_prior <- list(prior.point.normal(),prior.nonnegative())
+flash_prior <- list(prior.nonnegative(),prior.point.normal())
+# flash_prior <- list(prior.nonnegative(),prior.nonnegative())
+
 # Load the multinomial topic model fit.
 samples <- readRDS("../output/droplet/clustering-droplet.rds")
 fit <- readRDS("../output/droplet/rds/fit-droplet-scd-ex-k=7.rds")$fit
 
 # Fit flash model.
-fl <- flash(poisson2multinom(fit)$L,greedy.Kmax = 6,nullcheck = FALSE,
-                prior.family = prior.point.normal())
+X  <- poisson2multinom(fit)$L
+fl <- flash(X,greedy.Kmax = 6,nullcheck = FALSE,prior.family = flash_prior)
 colnames(fl$loadings.pm[[1]]) <- paste0("k",1:6)
 
 cluster_colors <- c("royalblue",   # B
@@ -35,4 +41,5 @@ Y  <- fl$loadings.pm[[1]]
 p1 <- labeled_pca_plot(Y,pcs = c("k1","k2"),samples$cluster)
 p2 <- labeled_pca_plot(Y,pcs = c("k3","k4"),samples$cluster)
 p3 <- labeled_pca_plot(Y,pcs = c("k5","k6"),samples$cluster)
-plot_grid(p1,p2,p3,nrow = 1)
+print(plot_grid(p1,p2,p3,nrow = 1))
+ggsave("plot.png",dpi = 150)
