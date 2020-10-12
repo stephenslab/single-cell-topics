70575e047f6d1007cc2e343a2ec7d591c6c1e166
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-10-11 23:35:17 -0500

    Improved scatterplots in compare_cell_loglik_droplet.R.

--- analysis/compare_cell_loglik_droplet.R
+++ analysis/compare_cell_loglik_droplet.R
@@ -23,7 +23,6 @@ loglik_topics  <- loglik_multinom_topic_model(counts,fit)
 loglik_cluster <- loglik_multinom_topic_model(counts,fit_cluster)
 loglik_montoro <- loglik_multinom_topic_model(counts,fit_montoro)
 
-# TO DO: Explain here what this function does, and how to use it.
 create_loglik_scatterplot <- function (dat, k, color = "black",
                                        minloglik = -Inf) {
   dat    <- subset(dat,loglik1 > minloglik & loglik2 > minloglik)
@@ -36,8 +35,8 @@ create_loglik_scatterplot <- function (dat, k, color = "black",
          xlim(limits) +
          ylim(limits) +
          labs(title = sprintf("%s (n = %d)",k,nrow(dat))) +
-         theme_cowplot(font_size = 10) +
-         theme(plot.title = element_text(size = 10,face = "plain")))
+         theme_cowplot(font_size = 9) +
+         theme(plot.title = element_text(size = 9,face = "plain")))
 }
 
 # Compare the two hard clusterings.
@@ -51,8 +50,12 @@ p3 <- create_loglik_scatterplot(dat,"Ciliated","firebrick",minloglik)
 p4 <- create_loglik_scatterplot(dat,"Neuroendocrine","darkorange",minloglik)
 p5 <- create_loglik_scatterplot(dat,"Tuft","dodgerblue",minloglik)
 p6 <- create_loglik_scatterplot(dat,"Goblet","gold",minloglik)
-plot_grid(p1,p2,p3,
-          p4,p5,p6,
+plot_grid(p1 + labs(x = "Montoro et al cluster",y = "our clusters"),
+          p2 + labs(x = "Montoro et al cluster",y = "our clusters"),
+          p3 + labs(x = "Montoro et al cluster",y = "our clusters"),
+          p4 + labs(x = "Montoro et al cluster",y = "our clusters"),
+          p5 + labs(x = "Montoro et al cluster",y = "our clusters"),
+          p6 + labs(x = "Montoro et al cluster",y = "our clusters"),
           nrow = 2,ncol = 3)
 
 # Compare the hard clustering against the topics.
@@ -65,15 +68,30 @@ p9  <- create_loglik_scatterplot(dat,"Cil","firebrick",minloglik)
 p10 <- create_loglik_scatterplot(dat,"H","turquoise",minloglik)
 p11 <- create_loglik_scatterplot(dat,"T+N","darkorange",minloglik)
 p12 <- create_loglik_scatterplot(dat,"G","gold",minloglik)
-plot_grid(p7,p8,p9,
-          p10,p11,p12,
+plot_grid(p7 + labs(x = "our cluster",y = "topics"),
+          p8 + labs(x = "our cluster",y = "topics"),
+          p9 + labs(x = "our cluster",y = "topics"),
+          p10 + labs(x = "our cluster",y = "topics"),
+          p11 + labs(x = "our cluster",y = "topics"),
+          p12 + labs(x = "our cluster",y = "topics"),
           nrow = 2,ncol = 3)
 
 # Compare the Montoro et al (2018) hard clustering against the topics.
 dat <- data.frame(cluster = samples$tissue,
                   loglik1 = loglik_montoro,
                   loglik2 = loglik_topics)
-p13 <- create_loglik_scatterplot(dat,"Neuroendocrine","darkorange",minloglik)
-p14 <- create_loglik_scatterplot(dat,"Tuft","dodgerblue",minloglik)
-p15 <- create_loglik_scatterplot(dat,"Ionocyte","darkmagenta",minloglik)
-plot_grid(p13,p14,p15,nrow = 1,ncol = 3)
+p13 <- create_loglik_scatterplot(dat,"Basal","royalblue",minloglik)
+p14 <- create_loglik_scatterplot(dat,"Club","forestgreen",minloglik)
+p15 <- create_loglik_scatterplot(dat,"Ciliated","firebrick",minloglik)
+p16 <- create_loglik_scatterplot(dat,"Neuroendocrine","darkorange",minloglik)
+p17 <- create_loglik_scatterplot(dat,"Tuft","dodgerblue",minloglik)
+p18 <- create_loglik_scatterplot(dat,"Ionocyte","darkmagenta",minloglik)
+p19 <- create_loglik_scatterplot(dat,"Goblet","gold",minloglik)
+plot_grid(p13 + labs(x = "Montoro et al cluster",y = "topics"),
+          p14 + labs(x = "Montoro et al cluster",y = "topics"),
+          p15 + labs(x = "Montoro et al cluster",y = "topics"),
+          p16 + labs(x = "Montoro et al cluster",y = "topics"),
+          p17 + labs(x = "Montoro et al cluster",y = "topics"),
+          p18 + labs(x = "Montoro et al cluster",y = "topics"),
+          p19 + labs(x = "Montoro et al cluster",y = "topics"),
+          nrow = 3,ncol = 3)
--- analysis/temp.R
+++
@@ -1,6 +0,0 @@
-library(uwot)
-set.seed(1)
-out <- umap(poisson2multinom(fit_droplet)$L,n_neighbors = 10,n_epochs = 1000,
-            min_dist = 0.1,scale = "none",learning_rate = 1,verbose = TRUE)
-colnames(out) <- c("d1","d2")
-pca_plot(fit_droplet,out.pca = list(x = out),fill = samples_droplet$cluster)
