956e422da16629cbd19eb6a820607044dcd73cc8
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-11-12 08:48:43 -0600

    Added comparison of PCA, UMAP and t-SNE to clusters_purified_pbmc.R.

--- analysis/clusters_purified_pbmc.R
+++ analysis/clusters_purified_pbmc.R
@@ -38,30 +38,64 @@ pc2  <- pca[,2]
 y[pc1 < -0.3 & 1.1*pc1 < -pc2 - 0.57] <- "NK"
 x[rows] <- y
 
-# Define (less distinct) CD8+ cluster.
-# TO DO.
-
-# Plot the top two PCs of the remaining cells.
-colors <- c("dodgerblue","forestgreen","greenyellow","magenta",
-            "firebrick","darkorange","gold","darkblue","darkmagenta","gray")
+# Define (less distinct) CD8+ cluster, and label the remaining cells
+# as T-cells.
 rows <- which(x == "U")
+n    <- length(rows)
 fit2 <- select(poisson2multinom(fit),loadings = rows)
-p1 <- pca_plot(fit2,fill = samples[rows,"celltype"],pcs = 1:2) +
-  scale_fill_manual(values = colors)
-p2 <- pca_plot(fit2,k = 1)
-p3 <- pca_plot(fit2,k = 4)
-p4 <- pca_plot(fit2,k = 6)
-plot_grid(p1,p2,p3,p4)
+pca  <- prcomp(fit2$L)$x
+y    <- rep("T",n)
+pc1  <- pca[,1]
+pc2  <- pca[,2]
+y[pc1 < 0.25 & pc2 < -0.15] <- "CD8+"
+x[rows] <- y
 
 # Compare our clusters with FACS cell-types.
 samples$cluster <- factor(x)
 with(samples,table(celltype,cluster))
 
+# Create PCA plots showing FACS cell-types.
+colors <- c("dodgerblue","forestgreen","greenyellow","magenta",
+            "firebrick","darkorange","gold","darkblue","darkmagenta","gray")
+x     <- with(samples,cluster == "T" | cluster == "CD8+")
+rows1 <- which(!x)
+rows2 <- which(x)
+p1 <- pca_plot(select(poisson2multinom(fit),loadings = rows1),
+               fill = samples[rows1,"celltype"]) +
+  scale_fill_manual(values = colors)
+p2 <- pca_plot(select(poisson2multinom(fit),loadings = rows2),
+               fill = samples[rows2,"celltype"]) +
+  scale_fill_manual(values = colors)
+plot_grid(p1,p2)
+
+set.seed(5)
+rows <- which(with(samples,
+                   cluster == "B" |
+                   cluster == "CD14+" |
+                   cluster == "CD34+"))
+rows <- sample(rows,2000)
+fit2 <- select(poisson2multinom(fit),loadings = rows)
+p3   <- pca_plot(fit2,fill = samples[rows,"cluster"])
+
+# Run t-SNE, then plot the 2-d embedding.
+library(Rtsne)
+tsne <- Rtsne(fit2$L,dims = 2,pca = FALSE,normalize = FALSE,perplexity = 100,
+              theta = 0.1,max_iter = 1000,eta = 200,verbose = TRUE)
+tsne$x <- tsne$Y
+colnames(tsne$x) <- c("tsne1","tsne2")
+p4 <- pca_plot(fit2,out.pca = tsne,fill = samples[rows,"cluster"])
+
+# Run UMAP, then plot the 2-d embedding.
+library(uwot)
+out.umap <- umap(fit2$L,n_neighbors = 30,metric = "euclidean",n_epochs = 1000,
+                 min_dist = 0.1,scale = "none",learning_rate = 1,
+                 verbose = TRUE)
+out.umap <- list(x = out.umap)
+colnames(out.umap$x) <- c("umap1","umap2")
+p5 <- pca_plot(fit2,out.pca = out.umap,fill = samples[rows,"cluster"])
+plot_grid(p3,p4,p5,nrow = 1)
+
 # TO DO:
-#
-#  + Create PCA plots summarizing clustering.
-#
-#  + Create PCA plots showing FACS cell-types.
 #
 #  + Create Structure Plot summarizing clustering.
 #
