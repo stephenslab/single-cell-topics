233d520ea7d80ebd63a3e42c5583cf9d74442b86
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-11-14 11:39:26 -0600

    Adjusted Y axis in create_progress_plots.

--- code/functions_for_assessing_fits.R
+++ code/functions_for_assessing_fits.R
@@ -32,13 +32,15 @@ create_progress_plots <- function (dat, fits, y = c("loglik","res")) {
   for (i in 2:k) {
     rows <- which(dat$k == i)
     plots[[i]] <-
-      plot_progress_poisson_nmf(fits[rows],x = "timing",y = y,
-                                add.point.every = 150,shapes = 21,
-                                colors = rep(c(clrs),2),
-                                fills = c(clrs,rep("white",3))) +
-      labs(x = "runtime (h)",title = paste("K =",i)) +
-      theme_cowplot(font_size = 10) +
-      theme(plot.title = element_text(size = 10,face = "plain"))
+      suppressMessages(
+        plot_progress_poisson_nmf(fits[rows],x = "timing",y = y,
+                                  add.point.every = 150,shapes = 21,
+                                  colors = rep(c(clrs),2),
+                                  fills = c(clrs,rep("white",3))) +
+        scale_y_continuous(trans = "log10",breaks = 10^seq(-8,8)) +
+        labs(x = "runtime (h)",title = paste("K =",i)) +
+        theme_cowplot(font_size = 10) +
+        theme(plot.title = element_text(size = 10,face = "plain")))
     if (i == 2)
       plots[[i]] <- plots[[i]] +
         theme(legend.position = c(0.95,0.95),
