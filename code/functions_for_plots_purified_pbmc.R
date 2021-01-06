a12c42a653f1e24b627dbb48b168d397e2260360
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-01-05 23:32:31 -0600

    Implemented function lfc_scatterplot in functions_for_plots_purified_pbmc.R.

--- code/functions_for_plots_purified_pbmc.R
+++ code/functions_for_plots_purified_pbmc.R
@@ -15,3 +15,41 @@ volcano_plot_better <- function (diff_count_res, k, labels,
     out <- out + guides(fill = "none")
   return(out)
 }
+
+# TO DO: Explain here what this function does, and how to use it.
+lfc_scatterplot <- function (res1, res2, k1, k2, labels, zmin = 6,
+                             label_above_quantile = 0.995,
+                             xlab = "", ylab = "") {
+  z1  <- abs(res1$Z[,k1])
+  z2  <- abs(res2$Z[,k2])
+  z0  <- quantile(z2,label_above_quantile)
+  dat <- data.frame(label = labels,
+                    mean  = res1$colmeans,
+                    x     = res1$beta[,k1],
+                    y     = res2$beta[,k2],
+                    stringsAsFactors = FALSE)
+  dat$mean <- cut(dat$mean,c(0,0.001,0.01,0.1,1,Inf))
+  dat$x    <- pmax(dat$x,-10)
+  dat$y    <- pmax(dat$y,-10)
+  i        <- which(dat$x >= 9.9)
+  dat$x[i] <- dat$x[i] + runif(length(i))
+  i        <- which(dat$y >= 9.9)
+  dat$y[i] <- dat$y[i] + runif(length(i))
+  dat$label[abs(z1) < z0 & abs(z2) < z0] <- ""
+  rows <- which(abs(res1$Z[,k1]) > zmin |
+                abs(res2$Z[,k2]) > zmin)
+  dat <- dat[rows,]
+  return(ggplot(dat,aes_string(x = "x",y = "y",fill = "mean",
+                               label = "label")) +
+         geom_point(shape = 21,color = "white") +
+         geom_abline(slope = 1,intercept = 0,linetype = "dotted") +
+         geom_text_repel(color = "black",size = 2,fontface = "italic",
+                         segment.color = "black",segment.size = 0.25,
+                         max.overlaps = Inf,na.rm = TRUE) +
+         scale_x_continuous(limits = c(-10,11),breaks = seq(-10,10,5)) +
+         scale_y_continuous(limits = c(-10,11),breaks = seq(-10,10,5)) +
+         scale_fill_manual(values = c("skyblue","cornflowerblue","orange",
+                                      "tomato","firebrick")) +
+         labs(x = xlab,y = ylab) +
+         theme_cowplot(font_size = 9))
+}
