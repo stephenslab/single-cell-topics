34066736219ef7c929fd60cf1b480d48e274937f
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-01-19 11:44:39 -0600

    Refactored the code for creating the scatterplots in matthews_idea_revisited.R.

--- analysis/matthews_idea_revisited.R
+++ analysis/matthews_idea_revisited.R
@@ -14,11 +14,8 @@ fit <- readRDS(file.path("../output/pbmc-purified/rds",
                          "fit-pbmc-purified-scd-ex-k=6.rds"))$fit
 samples <- readRDS("../output/pbmc-purified/clustering-pbmc-purified.rds")
 
-# Create a Structure plot in which the cells are grouped by FACS
-# cell population.
-topic_colors <- c("gold","forestgreen","dodgerblue","gray",
-                  "darkmagenta","violet")
-topics <- c(5,3,2,4,1,6)
+# Combine all the T cell subtypes, except for the CD8+ T cells, into a
+# single label.
 celltype <- as.character(samples$celltype)
 celltype[celltype == "CD4+ T Helper2" |
   celltype == "CD4+/CD45RO+ Memory" |
@@ -27,33 +24,31 @@ celltype[celltype == "CD4+ T Helper2" |
   celltype == "CD4+/CD25 T Reg"] <- "T cell"
 celltype <- factor(celltype)
 
-# For each cell type, calculate the average observed expression (X)
-# and the average predicted expression (Y). Store these calculations
-# in two m x k matrices, where m is the number of genes and k is the
-# number of cell types.
-m <- ncol(counts)
-k <- nlevels(celltype)
-X <- matrix(0,m,k)
-Y <- matrix(0,m,k)
-celltypes <- levels(celltype)
-for (j in 1:k) {
-  i     <- which(celltype == celltypes[j])
-  X[,j] <- colMeans(counts[i,])
-  Y[,j] <- with(fit,tcrossprod(colMeans(L[i,]),F))
-}
+# Create a scatterplot comparing, for each gene, the sample mean
+# expression against the mean predicted expression, where the mean is
+# taken over all samples in the selected cell type (k). Genes are
+# labeled if their mean expression is greater than obsmin and the
+# distance from the diagonal in the scatterplot (on the base-2
+# logarithmic scale) is greater than betamin.
+create_expression_scatterplot <- function (counts, fit, celltype, k,
+                                           labels = NULL, e = 0.001,
+                                           betamin = 1, obsmin = 0.1) {
 
-# TO DO: Explain what this function does, and how to use it.
-create_expression_scatterplot <- function (x, y, labels = NULL,
-                                           title = NULL, e = 0.001,
-                                           label_minbeta = 1,
-                                           label_minx = 0.1) {
-  x   <- x + e
-  y   <- y + e
+  # For each gene, compute the sample mean expression (x) and the
+  # predicted mean expression (y).
+  i   <- which(celltype == k)
+  x   <- colMeans(counts[i,]) + e
+  y   <- with(fit,drop(tcrossprod(colMeans(L[i,]),F))) + e
   dat <- data.frame(x = x,y = y,label = labels,stringsAsFactors = FALSE)
-  r   <- log2(pmax(x/y,y/x))
-  i   <- which(r < label_minbeta | x < label_minx)
+
+  # Label genes for which the mean expression is greater than
+  # "obsmin", and the distance from the diagonal on the (base-2)
+  # log-scale is greater than "betamin".
+  r <- log2(pmax(x/y,y/x))
+  i <- which(r < betamin | x < obsmin)
   dat[i,"label"] <- ""
-  
+
+  # Create the scatterplot.
   return(ggplot(dat,aes_string(x = "x",y = "y",label = "label")) +
          geom_point(color = "dodgerblue",size = 1) +
          geom_abline(slope = 1,intercept = 0,color = "black",
@@ -63,22 +58,23 @@ create_expression_scatterplot <- function (x, y, labels = NULL,
                          max.overlaps = Inf,na.rm = TRUE) +
          scale_x_continuous(trans = "log10",breaks = 10^seq(-4,4)) +
          scale_y_continuous(trans = "log10",breaks = 10^seq(-4,4)) +
-         labs(x = "observed",y = "predicted",title = title) +
+         labs(x = "observed",y = "predicted",title = k) +
          theme_cowplot(font_size = 10) +
          theme(plot.title = element_text(size = 10,face = "plain")))
 }
 
-# TO DO: Explain what these lines of code do.
-p1 <- create_expression_scatterplot(X[,1],Y[,1],genes$symbol,celltypes[1],
-                                    label_minbeta = 2,label_minx = 0.01)
-p2 <- create_expression_scatterplot(X[,2],Y[,2],genes$symbol,celltypes[2],
-                                    label_minbeta = 2,label_minx = 0.005)
-p3 <- create_expression_scatterplot(X[,3],Y[,3],genes$symbol,celltypes[3],
-                                    label_minbeta = 2,label_minx = 0.005)
-p4 <- create_expression_scatterplot(X[,4],Y[,4],genes$symbol,celltypes[4],
-                                    label_minbeta = 2,label_minx = 0.005)
-p5 <- create_expression_scatterplot(X[,5],Y[,5],genes$symbol,celltypes[5],
-                                    label_minbeta = 1.75,label_minx = 0.005)
-p6 <- create_expression_scatterplot(X[,6],Y[,6],genes$symbol,celltypes[6],
-                                    label_minbeta = 1.5,label_minx = 0.005)
+# Create 6 expression scatterplots, one for each of the FACS
+# subpopulations.
+p1 <- create_expression_scatterplot(counts,fit,celltype,"CD19+ B",
+                                    genes$symbol,betamin = 2,obsmin = 0.005)
+p2 <- create_expression_scatterplot(counts,fit,celltype,"CD14+ Monocyte",
+                                    genes$symbol,betamin = 2,obsmin = 0.01)
+p3 <- create_expression_scatterplot(counts,fit,celltype,"CD34+",
+                                    genes$symbol,betamin = 2,obsmin = 0.005)
+p4 <- create_expression_scatterplot(counts,fit,celltype,"CD56+ NK",
+                                    genes$symbol,betamin = 2,obsmin = 0.005)
+p5 <- create_expression_scatterplot(counts,fit,celltype,"CD8+ Cytotoxic T",
+                                    genes$symbol,betamin = 1.75,obsmin = 0.005)
+p6 <- create_expression_scatterplot(counts,fit,celltype,"T cell",
+                                    genes$symbol,betamin = 1.5,obsmin = 0.005)
 plot_grid(p1,p2,p3,p4,p5,p6,nrow = 2,ncol = 3)
