75edb6f9427948af2ff1753f49842b3c74002e70
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-01-19 13:25:58 -0600

    Revised scatterplots following Abhishek's suggestion to show multinomial probabilities (relative expression levels) instead of absolute expression levels to account for cell-to-cell differences in sequencing depth.

--- analysis/matthews_idea_revisited.R
+++ analysis/matthews_idea_revisited.R
@@ -9,10 +9,11 @@ library(cowplot)
 set.seed(1)
 
 # Load the data and results needed for this analysis.
-load("../data/pbmc_purified.RData")
+# load("../data/pbmc_purified.RData")
 fit <- readRDS(file.path("../output/pbmc-purified/rds",
                          "fit-pbmc-purified-scd-ex-k=6.rds"))$fit
 samples <- readRDS("../output/pbmc-purified/clustering-pbmc-purified.rds")
+fit <- poisson2multinom(fit)
 
 # Combine all the T cell subtypes, except for the CD8+ T cells, into a
 # single label.
@@ -24,30 +25,37 @@ celltype[celltype == "CD4+ T Helper2" |
   celltype == "CD4+/CD25 T Reg"] <- "T cell"
 celltype <- factor(celltype)
 
-# Create a scatterplot comparing, for each gene, the sample mean
-# expression against the mean predicted expression, where the mean is
-# taken over all samples in the selected cell type (k). Genes are
-# labeled if their mean expression is greater than obsmin and the
-# distance from the diagonal in the scatterplot (on the base-2
-# logarithmic scale) is greater than betamin.
+# Create a scatterplot comparing, for each gene, the
+# maximum-likelihood estimate for a simple multinomial model of
+# expression against the mean multinomial probability under a topic
+# model, in which the mean is taken over all samples in the selected cell
+# type (k). Genes are labeled if their multinomial MLE is greater than
+# mle.min and the distance from the diagonal in the scatterplot (on
+# the base-2 logarithmic scale) is greater than beta.min.
 create_expression_scatterplot <- function (counts, fit, celltype, k,
-                                           labels = NULL, e = 0.001,
-                                           betamin = 1, obsmin = 0.1) {
+                                           labels = NULL, e = 1e-6,
+                                           beta.min = 2, mle.min = 1e-6) {
 
-  # For each gene, compute the sample mean expression (x) and the
-  # predicted mean expression (y).
-  i   <- which(celltype == k)
-  x   <- colMeans(counts[i,]) + e
-  y   <- with(fit,drop(tcrossprod(colMeans(L[i,]),F))) + e
+  # Compute the maximum-likelihood estimate (MLE) for a basic
+  # multinomial model of gene expression for each cell.
+  i <- which(celltype == k)
+  x <- colSums(counts[i,])/sum(counts[i,]) + e
+
+  # For each gene, and for each cell, compute the multinomial
+  # probability under the given multinomial topic model, then take the
+  # average over all cells.
+  y <- drop(with(fit,tcrossprod(colMeans(L[i,]),F))) + e
+
+  # Create a data frame containing the data to be plotted.
   dat <- data.frame(x = x,y = y,label = labels,stringsAsFactors = FALSE)
 
-  # Label genes for which the mean expression is greater than
-  # "obsmin", and the distance from the diagonal on the (base-2)
-  # log-scale is greater than "betamin".
+  # Label genes for which the MLE is greater than mle.min, and the
+  # distance from the diagonal on the (base-2) log-scale is greater
+  # than beta.min.
   r <- log2(pmax(x/y,y/x))
-  i <- which(r < betamin | x < obsmin)
+  i <- which(r < beta.min | x < mle.min)
   dat[i,"label"] <- ""
-
+  
   # Create the scatterplot.
   return(ggplot(dat,aes_string(x = "x",y = "y",label = "label")) +
          geom_point(color = "dodgerblue",size = 1) +
@@ -56,25 +64,30 @@ create_expression_scatterplot <- function (counts, fit, celltype, k,
          geom_text_repel(color = "black",size = 2.2,fontface = "italic",
                          segment.color = "black",segment.size = 0.25,
                          max.overlaps = Inf,na.rm = TRUE) +
-         scale_x_continuous(trans = "log10",breaks = 10^seq(-4,4)) +
-         scale_y_continuous(trans = "log10",breaks = 10^seq(-4,4)) +
+         scale_x_continuous(trans = "log10",breaks = 10^seq(-8,0)) +
+         scale_y_continuous(trans = "log10",breaks = 10^seq(-8,0)) +
          labs(x = "observed",y = "predicted",title = k) +
          theme_cowplot(font_size = 10) +
          theme(plot.title = element_text(size = 10,face = "plain")))
 }
 
-# Create 6 expression scatterplots, one for each of the FACS
+# Create six expression scatterplots, one for each of the FACS
 # subpopulations.
 p1 <- create_expression_scatterplot(counts,fit,celltype,"CD19+ B",
-                                    genes$symbol,betamin = 2,obsmin = 0.005)
+                                    genes$symbol,beta.min = 1.25,
+                                    mle.min = 4e-6)
 p2 <- create_expression_scatterplot(counts,fit,celltype,"CD14+ Monocyte",
-                                    genes$symbol,betamin = 2,obsmin = 0.01)
+                                    genes$symbol,beta.min = 1.5,
+                                    mle.min = 1e-5)
 p3 <- create_expression_scatterplot(counts,fit,celltype,"CD34+",
-                                    genes$symbol,betamin = 2,obsmin = 0.005)
+                                    genes$symbol,beta.min = 1.5,mle.min = 1e-5)
 p4 <- create_expression_scatterplot(counts,fit,celltype,"CD56+ NK",
-                                    genes$symbol,betamin = 2,obsmin = 0.005)
+                                    genes$symbol,beta.min = 1.25,
+                                    mle.min = 4e-6)
 p5 <- create_expression_scatterplot(counts,fit,celltype,"CD8+ Cytotoxic T",
-                                    genes$symbol,betamin = 1.75,obsmin = 0.005)
+                                    genes$symbol,beta.min = 1.25,
+                                    mle.min = 1e-5)
 p6 <- create_expression_scatterplot(counts,fit,celltype,"T cell",
-                                    genes$symbol,betamin = 1.5,obsmin = 0.005)
+                                    genes$symbol,beta.min = 1.3,
+                                    mle.min = 4e-6)
 plot_grid(p1,p2,p3,p4,p5,p6,nrow = 2,ncol = 3)
