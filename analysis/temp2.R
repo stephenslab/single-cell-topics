01c11399743c5e2741dd6f6c45cd359b4dc43fdc
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-01-29 14:38:50 -0600

    Adding new code for volcano plots in plots_purified_pbmc analysis.

--- analysis/temp2.R
+++ analysis/temp2.R
@@ -1,2 +1,5 @@
 volcano_plot(diff_count_facs,k = "T cell",labels = genes$symbol,
              label_above_quantile = 0.995)
+volcano_plot(diff_count_topics,"k3",genes$symbol,
+             label_above_quantile = 0.998) +
+  scale_x_continuous(expand = expansion(mult = 0.5))
