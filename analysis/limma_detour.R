8eb8c88a3c8d8dd6ed2032a776158a4a56053c35
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-02-01 09:41:40 -0600

    Still working on limma_detour.R.

--- analysis/limma_detour.R
+++ analysis/limma_detour.R
@@ -1,6 +1,7 @@
 library(Matrix)
 library(fastTopics)
 library(limma)
+library(edgeR)
 set.seed(1)
 
 # Load the count data and K = 6 topic model fit.
@@ -19,31 +20,22 @@ counts  <- counts[,cols]
 fit     <- select_loadings(fit,loadings = rows)
 
 # Perform differential expression analysis using fastTopics.
-#
-# IDEA: Perform differential expression analysis for B-cells
-# vs. non-B-cells only.
-#
-celltype <- as.character(samples$celltype)
-celltype[celltype == "CD4+/CD45RA+/CD25- Naive T" |
-         celltype == "CD4+/CD45RO+ Memory" |
-         celltype == "CD8+/CD45RA+ Naive Cytotoxic" |
-         celltype == "CD4+ T Helper2" |
-         celltype == "CD4+/CD25 T Reg"] <- "T cell"
-celltype <- factor(celltype)
+celltype <- factor(samples$celltype == "CD19+ B")
+levels(celltype) <- c("A","B")
 res <- diff_count_clusters(celltype,counts)
 
 # Perform differential expression analysis using limma.
-# TO DO.
+dge    <- DGEList(t(counts))
+logCPM <- cpm(dge,log = TRUE,prior.count = 3)
+design <- model.matrix(~celltype)
+eb     <- lmFit(logCPM,design)
+eb     <- eBayes(eb,trend = TRUE)
+topTable(eb,number = 20)
 
 # Perform differential expression analysis using limma/voom.
-design <- model.matrix(~celltype)
-colnames(design) <- letters[1:6]
+design <- model.matrix(~0 + celltype)
 v <- voom(t(counts),design)
 v <- lmFit(v,design)
-contrast.matrix <- makeContrasts("b - (a + c + d + e + f)",
-                                 levels = design)
-v <- contrasts.fit(v,contrast.matrix)
 v <- eBayes(v)
-topTable(v,number = 20)
 plot(v$coefficients[,"celltypeCD19+ B"],res$beta[,"CD19+ B"],
      pch = 20,ylim = c(-10,10))
