b060be07a512ef41b9d8c57d12c690176b6e880a
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-02-16 22:18:22 -0600

    Made a few revisions to the examine_de_calculations.R script.

--- analysis/examine_de_calculations.R
+++ analysis/examine_de_calculations.R
@@ -3,6 +3,7 @@ library(Matrix)
 library(fastTopics)
 library(ggplot2)
 library(cowplot)
+set.seed(1)
 
 # Load the count data, the K = 6 topic model fit, and the 7 clusters
 # identified in the clustering analysis ("clusters_purified_pbmc").
@@ -12,22 +13,28 @@ fit <- readRDS(file.path("../output/pbmc-purified/rds",
 fit <- poisson2multinom(fit)
 samples <- readRDS("../output/pbmc-purified/clustering-pbmc-purified.rds")
 
+# Take a random subset of the cells.
+n       <- nrow(counts)
+rows    <- sample(n,9000)
+samples <- samples[rows,]
+counts  <- counts[rows,]
+fit     <- select_loadings(fit,loadings = rows)
+
 # Perform differential expression analysis using the multinomial topic
 # model, after removing the dendritic cells cluster.
-rows <- which(samples$cluster != "dendritic")
-fit_no_dendritic <- select_loadings(fit,loadings = rows)
-de <- diff_count_analysis(fit_no_dendritic,counts[rows,],
-                          shrink.method = "none")
+de <- diff_count_analysis(fit,counts,shrink.method = "none")
 
+# Plot z-score vs. total UMI count.
 i <- 5
 x <- colSums(counts)
 j <- which(x < 100)
 pdat <- data.frame(count = x[j],
                    z     = abs(de$Z[j,i]),
                    beta  = de$beta[j,i])
-ggplot(pdat,aes(x = count,y = z,fill = beta)) +
+p1 <- ggplot(pdat,aes(x = count,y = z,fill = beta)) +
   geom_point(shape = 21,color = "white") +
   scale_fill_gradient2(low = "lightskyblue",mid = "gold",high = "orangered",
                        midpoint = 0) +
   labs(x = "total UMI count",y = "|z-score|",fill = "logFC") +
   theme_cowplot(font_size = 12) 
+print(p1)
