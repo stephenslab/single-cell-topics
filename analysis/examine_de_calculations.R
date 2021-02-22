c495b7c5136b9fcdab4cda0bf581aca379dbae15
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-02-22 07:28:50 -0600

    Revised the examine_de_calculations.R script.

--- analysis/examine_de_calculations.R
+++ analysis/examine_de_calculations.R
@@ -1,9 +1,7 @@
-# TO DO: Explain here what this script is for, and how to use it.
 library(Matrix)
 library(fastTopics)
 library(ggplot2)
 library(cowplot)
-library(progress)
 set.seed(1)
 
 # Load the count data, the K = 6 topic model fit, and the 7 clusters
@@ -16,7 +14,7 @@ samples <- readRDS("../output/pbmc-purified/clustering-pbmc-purified.rds")
 
 # Perform differential expression analysis using the multinomial topic
 # model, after removing the dendritic cells cluster.
-rows <- sample(which(samples$cluster != "dendritic"),9000)
+rows <- which(samples$cluster != "dendritic")
 counts <- counts[rows,]
 fit <- select_loadings(fit,loadings = rows)
 de1 <- diff_count_analysis(fit,counts,pseudocount = 1,fit.method = "em")
@@ -26,10 +24,9 @@ fit2$F <- fit$F[j,]
 de2 <- diff_count_analysis(fit2,counts[,j],s = fit2$s,pseudocount = 1,
                            fit.method = "em")
 
-p1 <- volcano_plot(de1,"k3",genes$symbol,
-                   label_above_quantile = 0.998,
-                   subsample_below_quantile = 0.5)
-
-
 plot(de1$Z[j,],de2$Z,pch = 20)
 abline(a = 0,b = 1,col = "skyblue",lty = "dotted")
+
+p1 <- volcano_plot(de1,"k3",genes$symbol,betamax = 15,
+                   label_above_quantile = 0.998,
+                   subsample_below_quantile = 0.5)
