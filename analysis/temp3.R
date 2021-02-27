54894e2c68fda7d1b10f31bd9d2d79d291759823
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-02-26 22:12:18 -0600

    Working on gsea in plots_purified_pbmc analysis.

---
+++ analysis/temp3.R
@@ -0,0 +1,4 @@
+lfc_scatterplot(diff_count_clusters,diff_count_topics,"B","k3",
+                genes$symbol,label_above_lfc = 6,
+                label_above_quantile = 0.995,
+                xlab = "B cells cluster",ylab = "topic 3")
