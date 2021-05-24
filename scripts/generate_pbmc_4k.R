0853c468bf1da531f800c3cffefdd02292262fc6
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-05-24 14:51:53 -0500

    Revised generate_pbmc_4k.R script to include a test set.

--- scripts/generate_pbmc_4k.R
+++ scripts/generate_pbmc_4k.R
@@ -12,14 +12,24 @@ load("../data/pbmc_purified.RData")
 samples <- readRDS("../output/pbmc-purified/clustering-pbmc-purified.rds")
 
 # Select a random subset of the cells.
-x       <- samples$cluster
-i       <- c(sample(which(x == "T"),1212),
-             sample(which(x != "T" & x != "dendritic"),2562))
-i       <- sample(i)
-samples <- samples[i,]
-counts  <- counts[i,]
+x <- samples$cluster
+i <- c(sample(which(x == "T"),1212),
+       sample(which(x != "T" & x != "dendritic"),2562))
+j <- c(sample(setdiff(which(x == "T"),i),321),
+       sample(setdiff(which(x != "T" & x != "dendritic"),i),679))
+j <- sample(j)
+samples_test <- samples[j,]
+counts_test  <- counts[j,]
+samples      <- samples[i,]
+counts       <- counts[i,]
+
+# Remove genes with no expression in the training set.
+j           <- which(colSums(counts) > 0)
+genes       <- genes[i,]
+counts      <- counts[,j]
+counts_test <- counts_test[,j]
 
 # Save the data.
-save(list = c("samples","genes","counts"),
+save(list = c("samples","samples_test","genes","counts","counts_test"),
      file = "pbmc_4k.RData")
 resaveRdaFiles("pbmc_4k.RData")
