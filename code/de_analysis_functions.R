bb56afa5d01752131109b14d1283a2edd7a9c944
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-10-18 15:00:37 -0500

    Implemented function simulate_manytopic_umi_data.

--- code/de_analysis_functions.R
+++ code/de_analysis_functions.R
@@ -60,6 +60,55 @@ simulate_twotopic_umi_data <- function (m = 10000, s = 10^rnorm(200,0,0.2),
   return(list(X = X,s = s,F = F,L = L))
 }
 
+# TO DO: Explain here what this function does, and how to use it.
+simulate_manytopic_umi_data <- function (m = 10000, s = 10^rnorm(1000,0,0.2),
+                                         k = 6, p = 0.5) {
+
+  # Get the number of cells to simulate.
+  n <- length(s)
+    
+  # For each sample (row), generate the topic proportions.
+  L <- fastTopics:::generate_mixture_proportions(n,k)
+
+  # Generate the expression rates. The process for generating the
+  # expression rates F[i,j] is as follows: with probability 0.5, the
+  # rates F[i,j], for j = 1,...,k, are the same; otherwise, all the
+  # rates are the same except for a randomly chosen j, with
+  # (multiplicative, base-2) difference e. Here, y is normal with mean
+  # -4 and s.d., and e is normal with mean zero and s.d. 1.
+  #
+  # There might very well be more efficient ways to implement this
+  # step, but this current implementation in nonetheless fast enough
+  # for my purposes.
+  F <- matrix(0,m,k)
+  for (i in 1:m) {
+    y <- rnorm(1,-4,2)
+    e <- rnorm(1,0,1)
+    u <- runif(1)
+    j <- sample(k,1)
+    f <- rep(2^y,k)
+    if (u < p)
+      f[j] <- 2^(y + e)
+    F[i,] <- f
+  }
+
+  # Generate the counts from a Poisson NMF model with factors matrix F
+  # and loadings matrix s*L.
+  X <- fastTopics:::generate_poisson_nmf_counts(F,s*L)
+
+  # Return the counts matrix (X), the scaling factors (s), and the
+  # parameters of the Poisson NMF model used to simulate the data (F
+  # and L).
+  names(s)    <- paste0("c",1:n)
+  rownames(X) <- paste0("c",1:n)
+  colnames(X) <- paste0("g",1:m)
+  rownames(L) <- paste0("c",1:n)
+  colnames(L) <- paste0("k",1:k)
+  rownames(F) <- paste0("g",1:m)
+  colnames(F) <- paste0("k",1:k)
+  return(list(X = X,s = s,F = F,L = L))
+}
+
 # Create a data frame used to plot a power vs. FDR curve. Input
 # argument "true" is a logical vector in which true[i] is TRUE if and
 # only if the ith element is a true discovery; input argument "pval"
