171c5b1e96e7a29ee80b63618eab5d313bfb685d
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-11-12 16:00:33 -0600

    Added n script paramter to get_purified_pbmc_for_de.R.

--- scripts/get_purified_pbmc_for_de.R
+++ scripts/get_purified_pbmc_for_de.R
@@ -20,13 +20,14 @@ celltype[celltype == "CD4+ T Helper2" |
   celltype == "CD8+ Cytotoxic T"] <- "T cell"
 celltype <- factor(celltype)
 
-# Subsample the cell types so that there are at most 4,000 of each
+# Subsample the cell types so that there are at most n of each
 # cell type.
-i <- c(which(celltype == "CD14+ Monocyte"),
-       sample(which(celltype == "CD19+ B"),4000),
-       sample(which(celltype == "CD34+"),4000),
-       sample(which(celltype == "CD56+ NK"),4000),
-       sample(which(celltype == "T cell"),4000))
+n <- 800
+i <- c(sample(which(celltype == "CD14+ Monocyte"),n),
+       sample(which(celltype == "CD19+ B"),n),
+       sample(which(celltype == "CD34+"),n),
+       sample(which(celltype == "CD56+ NK"),n),
+       sample(which(celltype == "T cell"),n))
 i <- sort(i)
 celltype <- celltype[i]
 samples  <- samples[i,]
@@ -34,5 +35,5 @@ counts   <- counts[i,]
 
 # Save the new data set.
 save(list = c("genes","celltype","samples","counts"),
-     file = "pbmc_purified_for_de.RData")
+     file = "pbmc_purified_for_de_n=800.RData")
 resaveRdaFiles("pbmc_purified_for_de.RData")
