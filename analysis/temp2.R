baace76404e42d628fc68219eb54709b1fb84004
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-12-30 16:28:12 -0600

    Removed some R scripts that are no longer needed.

--- analysis/temp2.R
+++
@@ -1,5 +0,0 @@
-volcano_plot(diff_count_facs,k = "T cell",labels = genes$symbol,
-             label_above_quantile = 0.995)
-volcano_plot(diff_count_topics,"k3",genes$symbol,
-             label_above_quantile = 0.998) +
-  scale_x_continuous(expand = expansion(mult = 0.5))
--- analysis/temp3.R
+++
@@ -1,9 +0,0 @@
-library(Matrix)
-library(fastTopics)
-fit1 <- readRDS("../output/pbmc-68k/rds/fit-pbmc-68k-em-k=12.rds")$fit
-fit2 <- readRDS("../output/pbmc-68k/rds/fit-pbmc-68k-scd-ex-k=12.rds")$fit
-fit1 <- poisson2multinom(fit1)
-fit2 <- poisson2multinom(fit2)
-k <- 7
-plot(fit1$L[,k],fit2$L[,k],pch = 20)
-abline(a = 0,b = 1,col = "skyblue",lty = "dotted")
