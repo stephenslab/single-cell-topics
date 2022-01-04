793938349cd04876a8ef6f0576e5998419d5ac4e
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-01-04 08:47:13 -0600

    Added to de_analysis_eval.R plots comparing fastTopics::de_analysis and DESeq2 output.

--- analysis/de_analysis_eval.R
+++ analysis/de_analysis_eval.R
@@ -1,4 +1,7 @@
 library(Matrix)
+library(DESeq2)
+library(MAST)
+library(fastTopics)
 library(ggplot2)
 library(cowplot)
 source("../code/de_analysis_functions.R")
@@ -21,12 +24,12 @@ pdat <- data.frame(postmean1 = combine_sim_res(res,function (x) x$de1$postmean[,
 		   z2        = combine_sim_res(res,function (x) x$de2$z[,2]))
 p1 <- ggplot(pdat,aes(x = postmean1,y = postmean2)) +
   geom_point(shape = 4,size = 0.75) +
-  geom_abline(intercept = 0,slope = 1,color = "darkorange",linetype = "dashed") +
+  geom_abline(intercept = 0,slope = 1,color = "magenta",linetype = "dashed") +
   labs(x = "first posterior mean",y = "second posterior mean") +
   theme_cowplot(font_size = 12)
 p2 <- ggplot(pdat,aes(x = z1,y = z2)) +
   geom_point(shape = 4,size = 0.75) +
-  geom_abline(intercept = 0,slope = 1,color = "darkorange",linetype = "dashed") +
+  geom_abline(intercept = 0,slope = 1,color = "magenta",linetype = "dashed") +
   labs(x = "first z-score estimate",y = "second z-score estimate") +
   xlim(-45,42) + 
   ylim(-45,42) + 
@@ -35,3 +38,41 @@ p2 <- ggplot(pdat,aes(x = z1,y = z2)) +
 # Save the plots.
 ggsave("../plots/mcmc_scatterplots_sims_k=2_alpha=0.01.png",
        plot_grid(p1,p2),height = 3,width = 6,dpi = 600)
+
+# Compare DESeq2 and fastTopics output.
+pdat1 <- data.frame(lfc.deseq      = combine_sim_res(res,function (x) x$deseq$log2FoldChange),
+                    lfc.fasttopics = combine_sim_res(res,function (x) x$de1$postmean[,2]),
+                    z.deseq        = combine_sim_res(res,
+                                       function (x) with(x$deseq,log2FoldChange/lfcSE)),
+                    z.fasttopics   = combine_sim_res(res,function (x) x$de1$z[,2]))
+pdat2 <- data.frame(deseq2 = combine_sim_res(res,function (x) x$deseq$svalue),
+                    shrink = combine_sim_res(res,function (x) x$de1$svalue[,2]),
+                    de     = factor(combine_sim_res(res,
+                               function (x) with(x$dat,abs(F[,1] - F[,2]) > 1e-8))))
+p1 <- ggplot(pdat1,aes(x = lfc.deseq,y = lfc.fasttopics)) +
+  geom_point(shape = 4,size = 0.75) +
+  geom_abline(intercept = 0,slope = 1,color = "magenta",linetype = "dashed") +
+  labs(x = "DESeq2",y = "GoM DE",title = "LFC estimates") +
+  theme_cowplot(font_size = 12)
+p2 <- ggplot(pdat1,aes(x = z.deseq,y = z.fasttopics)) +
+  geom_point(shape = 4,size = 0.75) +
+  geom_abline(intercept = 0,slope = 1,color = "magenta",linetype = "dashed") +
+  labs(x = "DESeq2",y = "GoM DE",title = "z-scores") +
+  theme_cowplot(font_size = 12)
+p3 <- ggplot(pdat2,aes(x = deseq2,color = de,fill = de)) +
+  geom_histogram(bins = 64,show.legend = FALSE) +
+  scale_color_manual(values = c("darkorange","darkblue")) +
+  scale_fill_manual(values = c("darkorange","darkblue")) +
+  labs(x = "s-value",y = "genes",title = "DESeq2") +
+  theme_cowplot(font_size = 12)
+p4 <- ggplot(pdat2,aes(x = shrink,color = de,fill = de)) +
+  geom_histogram(bins = 64,show.legend = FALSE) +
+  scale_color_manual(values = c("darkorange","darkblue")) +
+  scale_fill_manual(values = c("darkorange","darkblue")) +
+  labs(x = "s-value",y = "genes",title = "fastTopics") +
+  theme_cowplot(font_size = 12)
+       
+# Save the plots.
+ggsave("../plots/deseq2_vs_fasttopics_sims.png",
+       plot_grid(p1,p2,p3,p4,nrow = 2,ncol = 2),
+       height = 3,width = 9,dpi = 600)
