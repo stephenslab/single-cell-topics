0e3f3449a8cdd5c7bc5c4a28403af16b80270b88
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-01-04 08:06:19 -0600

    Added step to load simulation results in de_analysis_eval.R.

---
+++ analysis/de_analysis_eval.R
@@ -0,0 +1,8 @@
+library(Matrix)
+library(ggplot2)
+library(cowplot)
+
+# Load the results of the simulations when simulated topic proportions
+# are mostly 0 or 1.
+load("../output/sims/sims-k=2-alpha=0.01.RData")
+
