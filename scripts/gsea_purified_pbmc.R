1de57705ff2cf4eb6328302856120687061066d3
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-01-26 09:23:09 -0600

    Made a few small improvements to gsea_purified_pbmc.R script.

--- scripts/gsea_purified_pbmc.R
+++ scripts/gsea_purified_pbmc.R
@@ -1,19 +1,31 @@
 # TO DO: Explain here what this script does, and how to use it.
+#
+# These were the steps taken to load R and allocate computing
+# resources for this analysis:
+#
+#   sinteractive -p broadwl -c 20 --mem=16G --time=10:00:00
+#   module load R/4.1.0
+#
 library(Matrix)
 library(fgsea)
 library(iDEA)
+library(tools)
 source("../code/gsea.R")
-set.seed(1)
 
-# Script parameters
-k <- "k3"
+# Script parameters.
+k       <- "k3"
+outfile <- "gsea-pbmc-purified-k=3.RData"
+print(k)
+print(outfile)
+set.seed(1)
 
 # Load the gene sets.
 load("../data/gene_sets_human.RData")
 gene_sets <- gene_sets_human$gene_sets
 
 # *** TESTING ***
-gene_sets <- as.matrix(gene_sets[,1:20])
+i         <- sample(ncol(gene_sets),20)
+gene_sets <- as.matrix(gene_sets[,i])
 
 # Load the results of the DE analysis, and create the summary data
 # object, a data frame with two rows: (1) the LFC estimates, and (2)
@@ -66,4 +78,5 @@ idea <- iDEA.fit(idea,min_degene = 4,em_iter = 20,mcmc_iter = 1000,
 idea <- iDEA.louis(idea)
 
 # Save the results to file.
-# TO DO.
+save(list = c("out.fgsea","idea"),file = outfile)
+resaveRdaFiles(outfile)
