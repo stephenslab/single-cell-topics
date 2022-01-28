736c130eb829728cfd5113b61f6af93982db8ab2
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-01-28 15:39:51 -0600

    Revised the de_analysis_droplet.R script.

--- scripts/de_analysis_droplet.R
+++ scripts/de_analysis_droplet.R
@@ -3,7 +3,7 @@
 # data, with k = 7 topics. These were the steps taken to load R and
 # allocate computing resources for this analysis:
 #
-#   sinteractive -p broadwl -c 20 --mem=16G --time=60:00:00
+#   sinteractive -p broadwl -c 20 --mem=16G --time=10:00:00
 #   module load R/3.5.1
 #
 
@@ -25,7 +25,7 @@ fit <- poisson2multinom(fit)
 # Perform the DE analysis.
 t0 <- proc.time()
 de <- de_analysis(fit,counts,pseudocount = 0.1,
-                  control = list(ns = 1000,nc = 20,nsplit = 100))
+                  control = list(ns = 1e5,nc = 20,nsplit = 1000))
 t1 <- proc.time()
 timing <- t1 - t0
 cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
@@ -34,12 +34,12 @@ cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
 t0 <- proc.time()
 fit_merged <- merge_topics(fit,c("k5","k7"))
 de_merged <- de_analysis(fit_merged,counts,pseudocount = 0.1,
-                         control = list(ns = 1000,nc = 20,nsplit = 100))
+                         control = list(ns = 1e5,nc = 20,nsplit = 1000))
 t1 <- proc.time()
 timing <- t1 - t0
 cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
 
 # Save the results.
-save(list = c("genes","de","de_merged"),
+save(list = c("de","de_merged"),
      file = "de-droplet.RData")
 resaveRdaFiles("de-droplet.RData")
