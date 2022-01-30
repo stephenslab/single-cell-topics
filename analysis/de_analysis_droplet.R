af26a025efbc4a0b34f05fa8a985785ae3f9f83a
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-01-29 22:14:09 -0600

    Small edit.

--- analysis/de_analysis_droplet.R
+++ analysis/de_analysis_droplet.R
@@ -13,5 +13,5 @@ p4 <- volcano_plot(de_merged,ymax = 200,k = "k4") # Hillocks
 p5 <- volcano_plot(de_merged,ymax = 200,k = "k5+k7") # Club cells
 p6 <- volcano_plot(de_merged,ymax = 100,k = "k6") # Ciliated cells
 p7 <- volcano_plot(de,k = "k5",ymax = 200) # Other club cells
-p8 <- volcano_plot(de,k = "k7",ymax = 200) # Scgb1a1+ club cells
+p8 <- volcano_plot(de,k = "k7",ymax = 200) # Scgb1a1+ 
 plot_grid(p7,p8)
