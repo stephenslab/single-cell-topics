e2e01aad054779fcf31d5dfc9d81c0bd1316a63a
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-02-10 12:11:27 -0600

    Revised the refit_poisson_nmf_droplet.R script.

--- scripts/refit_poisson_nmf_droplet.R
+++ scripts/refit_poisson_nmf_droplet.R
@@ -1,4 +1,15 @@
-# TO DO: Explain here what this script does, and how to use it.
+# Based on the topic modeling results in the droplet data, with k = 7
+# topics, one of the topics, topic 6, appears to be capturing rare,
+# specialized epithelial cells, including ionocyte, tuft and
+# neuroendocrine cells. Therefore, here we reanalyze the cells with
+# membership to topic 6.
+#
+# These were the steps taken to load R and allocate computing
+# resources for this analysis:
+#
+#   sinteractive -p broadwl -c 20 --mem=16G --time=6:00:00
+#   module load R/3.5.1
+#
 
 # Load a couple packages.
 library(Matrix)
@@ -10,14 +21,15 @@ set.seed(1)
 # Load the previously prepared count data.
 load("../data/droplet.RData")
 
-# Select cells that have at least 10% membership to topic 6.
+# Select cells that have at least 10% membership in topic 6.
 fit     <- readRDS("../output/droplet/rds/fit-droplet-scd-ex-k=7.rds")$fit
 fit     <- poisson2multinom(fit)
 rows    <- which(fit$L[,6] > 0.1)
 samples <- samples[rows,]
 counts  <- counts[rows,]
 
-# Now we are ready to perform the main model-fitting step.
+# Now we are ready to perform the main model-fitting step. We repeat
+# the model fitting for k = 2 through 6.
 fits        <- vector("list",6)
 names(fits) <- paste0("k",1:6)
 for (k in 2:6)
@@ -27,18 +39,16 @@ for (k in 2:6)
                                               numiter = 4,nc = 4))
 fits <- fits[-1]
 
-plot_loglik_vs_rank(fits)
-
-stop()
-
-# Perform the DE analysis.
+# Perform a DE analysis using the topic model with k = 5 topics.
 t0 <- proc.time()
-de <- de_analysis(fit,counts,pseudocount = 0.1,control = list(ns = 1e4,nc = 4))
+fit <- poisson2multinom(fits$k5)
+de <- de_analysis(fit,counts,pseudocount = 0.1,control = list(ns = 1e3,nc = 4))
 t1 <- proc.time()
 timing <- t1 - t0
 cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
 
-# Perform a second DE analysis after merging topics 1 and 3.
+# Perform a second DE analysis with the k = 5 topic model after
+# merging topics 2 and 4.
 t0 <- proc.time()
 fit_merged <- merge_topics(fit,c("k1","k3"))
 de_merged <- de_analysis(fit_merged,counts,pseudocount = 0.1,
@@ -49,4 +59,5 @@ cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
 
 # SAVE RESULTS
 # ------------
-saveRDS(list(fit = fit),file = "refit-droplet-scd-ex=k=7.rds")
+saveRDS(list(fits = fits,de = de,de_merged = de_merged),
+        file = "refit-droplet-scd-ex=k=7.rds")
