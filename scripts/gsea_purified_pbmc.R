fff7dc28451feba14c71a1a8728d0f7f1c73a4ba
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-05-13 15:06:14 -0500

    Working on new gsea script for pbmc data using susieR.

--- scripts/gsea_purified_pbmc.R
+++ scripts/gsea_purified_pbmc.R
@@ -4,100 +4,54 @@
 # resources for this analysis:
 #
 #   sinteractive -p broadwl -c 20 --mem=36G --time=60:00:00
-#   module load R/4.1.0
+#   module load R/3.5.1
 #
 library(Matrix)
-library(fgsea)
-library(iDEA)
 library(tools)
-source("../code/gsea.R")
-
-# Script parameters.
-k       <- "k3"
-outfile <- "gsea-pbmc-purified-k=3.RData"
-print(k)
-print(outfile)
+library(susieR)
+library(pathways)
 set.seed(1)
 
 # Load the gene sets.
-load("../data/gene_sets_human.RData")
-gene_sets <- gene_sets_human$gene_sets
+data(gene_sets_human)
+X <- gene_sets_human$gene_sets
 
-# Load the results of the DE analysis, and create the summary data
-# object, a data frame with two rows: (1) the LFC estimates, and (2)
-# the variances of the LFC estimates.
+# Load the results of the DE analysis.
 load("../output/pbmc-purified/de-pbmc-purified-seed=1.RData")
-zscores <- de$z[,k]
-sdat    <- with(de,data.frame(log2FoldChange = postmean[,k],
-                              lfcSE2         = (postmean[,k]/z[,k])^2))
-i       <- which(!(is.na(zscores) | is.na(sdat$lfcSE2)))
-zscores <- zscores[i]
-sdat    <- sdat[i,]
+Y <- de$postmean
 
-# Align the gene-set data with the gene-wise statistics.
-ids <- intersect(rownames(gene_sets),rownames(sdat))
-i   <- match(ids,rownames(gene_sets))
-j   <- match(ids,rownames(sdat))
-gene_sets <- gene_sets[i,]
-sdat      <- sdat[j,]
-zscores   <- zscores[j]
-
-# Remove gene sets in several collections that aren't relevant.
+# Remove gene sets in several MSigDB collections that clearly aren't
+# relevant.
 i <- which(!is.element(gene_sets_human$gene_set_info$database,
                        c("MSigDB-ARCHIVED","MSigDB-C1","MSigDB-C3",
                          "MSigDB-C4","MSigDB-C6")))
-gene_sets <- gene_sets[,i]
+X <- X[,i]
 
-# Next, remove gene sets with fewer than 10 genes, and with more than
+# Align the gene-set data with the gene-wise statistics.
+ids <- intersect(rownames(X),rownames(Y))
+X <- X[ids,]
+Y <- Y[ids,]
+
+# Next, remove gene sets with fewer than 10 genes and with more than
 # 400 genes. Gene sets with a large number of genes are less likely to
 # be interesting, and slow down the enrichment analysis, so they are
 # removed.
-i <- which(colSums(gene_sets) >= 10 & colSums(gene_sets) <= 400)
-gene_sets <- gene_sets[,i]
+i <- which(colSums(X) >= 10 & colSums(X) <= 400)
+X <- X[,i]
 
-# Convert the sparse matrix representation of the gene sets to a list.
-gene_sets <- matrix2list(gene_sets)
-
-# Perform a gene set enrichment analysis using fgsea.
+# Perform a gene set enrichment analysis using susieR.
+k <- ncol(Y)
+gsea <- vector("list",k)
+names(gsea) <- colnames(Y)
 t0 <- proc.time()
-out <- fgsea(lapply(gene_sets,function (x) names(x)),zscores,
-             eps = 1e-32,nproc = 20)
-class(out)    <- "data.frame"
-rownames(out) <- names(gene_sets)
-out <- out[c("pval","padj","log2err","ES","NES","size")]
-out <- out[names(gene_sets),]
-out[is.na(out$log2err),] <- NA
-out.fgsea <- out
-t1 <- proc.time()
-timing <- t1 - t0
-cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
-
-stop()
-
-# Perform a gene set enrichment analysis using iDEA.
-t0 <- proc.time()
-idea <- new(Class      = "iDEA",
-            gene_id    = rownames(sdat),
-            annot_id   = names(gene_sets),
-            summary    = sdat,
-            annotation = gene_sets,
-            num_gene   = nrow(sdat),
-            num_core   = 20,
-            project    = "idea")
-# Error in names(res_idea) <- object@annot_id :
-#   attempt to set an attribute on NULL
-set.seed(1)
-idea1 <- iDEA.fit(idea,min_degene = 4,em_iter = 10,mcmc_iter = 100, 
- 	          fit.tol = 1e-6,modelVariant = TRUE,verbose = TRUE)
-set.seed(1)
-idea2 <- iDEA.fit(idea,min_degene = 4,em_iter = 10,mcmc_iter = 100, 
- 	          fit.tol = 1e-6,modelVariant = TRUE,verbose = TRUE)
-idea <- iDEA.louis(idea)
+for (i in 1:k)
+  gsea[[i]] <- susie(X,Y[,i],L = 10,intercept = TRUE,standardize = FALSE,
+                     estimate_residual_variance = TRUE,refine = FALSE,
+                     compute_univariate_zscore = FALSE,verbose = TRUE)
 t1 <- proc.time()
 timing <- t1 - t0
 cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
 
 # Save the results to file.
-idea_gsea <- idea@gsea
-save(list = c("out.fgsea","idea_gsea"),file = outfile)
-resaveRdaFiles(outfile)
+save(list = "gsea",file = "gsea-pbmc-purified.RData")
+resaveRdaFiles("gsea-pbmc-purified.RData")
