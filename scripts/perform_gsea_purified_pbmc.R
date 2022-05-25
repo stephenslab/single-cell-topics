64b3446273e81bc6f420faadc1fa6a429b972444
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-05-25 09:29:40 -0500

    Revising perform_gsea_purified_pbmc.R script.

---
+++ scripts/perform_gsea_purified_pbmc.R
@@ -0,0 +1,67 @@
+# A short script used to perform the gene set enrichment analysis
+# using the GoM DE analysis results on the purified PBMC data, with 
+# k = 6 topics.
+library(Matrix)
+library(tools)
+library(susieR)
+library(pathways)
+
+# Initialize the sequence of pseudorandom numbers.
+set.seed(1)
+
+# Load the gene sets.
+data(gene_sets_human)
+X <- gene_sets_human$gene_sets
+
+# Load the results of the topic-model-based DE analysis.
+load("../output/pbmc-purified/de-pbmc-purified-seed=1.RData")
+de1 <- de
+load("../output/pbmc-purified/de-pbmc-purified-seed=2.RData")
+de2 <- de
+rm(de)
+
+# When the two z-scores disagree, use the one that is nearer to zero.
+de             <- de1[c("postmean","z")]
+i              <- which(abs(de2$z) < abs(de1$z))
+de$postmean[i] <- de2$postmean[i]
+de$z[i]        <- de2$z[i]
+Y              <- de$postmean
+
+# Remove gene sets in several MSigDB collections that clearly aren't
+# relevant.
+i <- which(!is.element(gene_sets_human$gene_set_info$database,
+                       c("MSigDB-ARCHIVED","MSigDB-C1","MSigDB-C3",
+                         "MSigDB-C4","MSigDB-C6")))
+X <- X[,i]
+
+# Align the gene-set data with the gene-wise statistics.
+ids <- intersect(rownames(X),rownames(Y))
+X <- X[ids,]
+Y <- Y[ids,]
+
+# Next, remove gene sets with fewer than 10 genes and with more than
+# 400 genes. Gene sets with a large number of genes are less likely to
+# be interesting, and slow down the enrichment analysis, so they are
+# removed.
+i <- which(colSums(X) >= 10 & colSums(X) <= 400)
+X <- X[,i]
+
+# Perform a gene set enrichment analysis using susieR.
+topics <- colnames(Y)
+gsea <- vector("list",ncol(Y))
+names(gsea) <- topics
+t0 <- proc.time()
+for (i in topics) {
+  cat("topic",i,"\n")
+  out <- susie(X,Y[,i],L = 10,intercept = TRUE,standardize = FALSE,
+               estimate_residual_variance = TRUE,refine = FALSE,
+               compute_univariate_zscore = FALSE,verbose = TRUE,
+               min_abs_corr = 0)
+}
+t1 <- proc.time()
+timing <- t1 - t0
+cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
+
+# Save the results to file.
+save(list = c("X","Y","gsea"),file = "gsea-pbmc-purified.RData")
+resaveRdaFiles("gsea-pbmc-purified.RData")
